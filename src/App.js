328a792c6520bacab8a23158538d87540e639568
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-16 17:22:23 -0400

    react-toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import ReactIcons from './components/react-icons/ReactIcons';
+import ReactToastify from './components/react-toastify/ReactToastify';
 
 function App() {
   return (
     <div className='App'>
       <h1 style={{ color: 'red' }}>REACT ICONS</h1>
       <ReactIcons />
+      <h1 style={{ color: 'red' }}>REACT TOASTIFY</h1>
+      <ReactToastify />
     </div>
   );
 }
