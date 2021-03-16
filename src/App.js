45c5009802f7b051179abb585f1b9c28a65d527c
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-16 16:52:42 -0400

    react-icons

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactIcons from './components/react-icons/ReactIcons';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 style={{ color: 'red' }}>REACT ICONS</h1>
+      <ReactIcons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/react-icons/ReactIcons.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IconContext } from 'react-icons';
+import { GiAbstract018 } from 'react-icons/gi';
+import { ImOffice } from 'react-icons/im';
+
+function ReactIcons() {
+  return (
+    <IconContext.Provider value={{ color: 'blue', size: '3rem' }}>
+      <div>
+        <GiAbstract018 />
+        <ImOffice />
+        {/* <GiAbstract018 color='purple' size='10rem' />
+        <ImOffice color='purple' size='10rem' /> */}
+      </div>
+    </IconContext.Provider>
+  );
+}
+
+export default ReactIcons;
